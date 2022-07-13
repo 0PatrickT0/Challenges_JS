9880a5ac434cd2e484b196787ee8aa41238ba208
0PatrickT0 <95145049+0PatrickT0@users.noreply.github.com>
2022-07-13 14:02:09 +0200

    Challenge 5

--- script.js
+++ script.js
@@ -1,55 +1,21 @@
 "use strict";
 
-// Challenge 4
+// Challenge 5
 
-const dauphinsScoreMatch1Text = prompt("Match1, score Dauphins ?");
-const dauphinsScoreMatch1 = parseInt(dauphinsScoreMatch1Text);
-const dauphinsScoreMatch2Text = prompt("Match2, score Dauphins ?");
-const dauphinsScoreMatch2 = parseInt(dauphinsScoreMatch2Text);
-const dauphinsScoreMatch3Text = prompt("Match3, score Dauphins ?");
-const dauphinsScoreMatch3 = parseInt(dauphinsScoreMatch3Text);
+let tauxPourboire = 0
+let addition = prompt("Montant de la note ?");
+addition = parseFloat(addition);
 
-const koalasScoreMatch1Text = prompt("Match1, score Koalas ?");
-const koalasScoreMatch1 = parseInt(koalasScoreMatch1Text);
-const koalasScoreMatch2Text = prompt("Match2, score Koalas ?");
-const koalasScoreMatch2 = parseInt(koalasScoreMatch2Text);
-const koalasScoreMatch3Text = prompt("Match3, score Koalas ?");
-const koalasScoreMatch3 = parseInt(koalasScoreMatch3Text);
+//if (addition >=50 && addition <=300) {
+//    tauxPourboire = 0.15;
+//} else {
+//    tauxPourboire = 0.20;
+//}
 
-function scoresAverage(donnée1, donnée2, donnée3) {
-  return (donnée1 + donnée2 + donnée3) / 3;
-}
+addition >=50 && addition <=300 ? tauxPourboire = 0.15 : tauxPourboire = 0.20;
 
-const dauphinsScoresAverage = scoresAverage(
-  dauphinsScoreMatch1,
-  dauphinsScoreMatch2,
-  dauphinsScoreMatch3
-);
-console.log(dauphinsScoresAverage.toFixed(0));
+let pourboire = (addition*tauxPourboire);
+let factureTotale = (addition + pourboire);
 
-const koalasScoresAverage = scoresAverage(
-  koalasScoreMatch1,
-  koalasScoreMatch2,
-  koalasScoreMatch3
-);
-console.log(koalasScoresAverage.toFixed(0));
-
-if (dauphinsScoresAverage > koalasScoresAverage) {
-  console.log("Victoire des Dauphins");
-} else if (koalasScoresAverage > dauphinsScoresAverage) {
-  console.log("Victoire des Koalas");
-} else {
-  console.log("Egalité");
-}
-
-if (dauphinsScoresAverage>100 || koalasScoresAverage>100) {
-    if (dauphinsScoresAverage > koalasScoresAverage) {
-    console.log("Victoire des Dauphins");
-    } else if (koalasScoresAverage > dauphinsScoresAverage) {
-    console.log("Victoire des Koalas");
-    } else {
-    console.log("Egalité");
-    }
-} else {
-    console.log("Pas de gagnant");
-}
+console.log(factureTotale)
+console.log(`La note était de ${addition.toFixed(2)}, le pourboire de ${pourboire.toFixed(2)} et la valeur totale était de ${factureTotale.toFixed(2)}`);
