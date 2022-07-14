fa910ba38e613500562c5516312373f497f3f484
0PatrickT0 <95145049+0PatrickT0@users.noreply.github.com>
2022-07-14 15:21:29 +0200

    Challenge 6

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 "use strict";
 
-// Challenge 5
+// Challenge 6
 
-let tauxPourboire = 0
-let addition = prompt("Montant de la note ?");
-addition = parseFloat(addition);
+const dauphinsScores = [85, 54, 41];
+const koalasScores = [23, 34, 27];
 
-//if (addition >=50 && addition <=300) {
-//    tauxPourboire = 0.15;
-//} else {
-//    tauxPourboire = 0.20;
-//}
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-addition >=50 && addition <=300 ? tauxPourboire = 0.15 : tauxPourboire = 0.20;
+let dauphinsScoreAverage = calcAverage(
+  dauphinsScores[0],
+  dauphinsScores[1],
+  dauphinsScores[2]
+);
+let koalasScoreAverage = calcAverage(
+  koalasScores[0],
+  koalasScores[1],
+  koalasScores[2]
+);
 
-let pourboire = (addition*tauxPourboire);
-let factureTotale = (addition + pourboire);
+function checkWinner() {
+  if (dauphinsScoreAverage > koalasScoreAverage) {
+    return `L'équipe dauphins gagne (${dauphinsScoreAverage} vs ${koalasScoreAverage})`;
+  } else {
+    return `L'équipe koalas gagne (${koalasScoreAverage} vs ${dauphinsScoreAverage})`;
+  }
+}
 
-console.log(factureTotale)
-console.log(`La note était de ${addition.toFixed(2)}, le pourboire de ${pourboire.toFixed(2)} et la valeur totale était de ${factureTotale.toFixed(2)}`);
+console.log(checkWinner());
